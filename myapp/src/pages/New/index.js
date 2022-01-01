80d1b523c1ae087b3e6ed71ce181a39c0f15d453
shuaiqidechuan <1335917149@qq.com>
2022-01-01 12:42:54 +0800

    nothing

---
+++ myapp/src/pages/New/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Button } from "antd"
+
+import styles from './index.less'
+
+
+const New = () => {
+    return <div>
+        new page
+    </div>
+};
+
+export default New;
